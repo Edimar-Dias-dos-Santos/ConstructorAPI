77e3135782b180409c979b48dce1463c53ffe6af
Edimar <eduardo.santos@useall.com.br>
2023-06-26 16:39:17 -0300

    ajustes necessarios para consumir no front

--- src/main/java/com/constructor/constructorAPI/controllers/UsuarioPerfilImgController.java
+++ src/main/java/com/constructor/constructorAPI/controllers/UsuarioPerfilImgController.java
@@ -62,6 +62,73 @@ public class UsuarioPerfilImgController {
         }
     }
 
+    @PutMapping("/{idUsuarioPerfilImg}")
+    public ResponseEntity<UsuarioPerfilImg> updateUserProfileImage(@PathVariable UUID idUsuarioPerfilImg, @RequestParam("image") MultipartFile image) {
+        Optional<UsuarioPerfilImg> optionalUserProfileImg = repUsuarioPerfilImg.findById(idUsuarioPerfilImg);
+        if (optionalUserProfileImg.isPresent()) {
+            UsuarioPerfilImg userProfileImg = optionalUserProfileImg.get();
+
+            // Deletar o arquivo anterior correspondente à imagem do diretório
+            File previousFile = new File(userProfileImg.getUrl());
+            if (previousFile.exists()) {
+                previousFile.delete();
+            }
+
+            String originalFilename = image.getOriginalFilename();
+            String filename = getUniqueFilename(originalFilename);
+            String filepath = UPLOAD_DIR + File.separator + filename;
+
+            try {
+                Path destination = Path.of(filepath);
+                Files.copy(image.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+
+                userProfileImg.setUrl(filepath);
+                repUsuarioPerfilImg.save(userProfileImg);
+
+                return ResponseEntity.ok().body(userProfileImg);
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{idUsuarioPerfilImg}")
+    public ResponseEntity<Void> deleteUserProfileImage(@PathVariable UUID idUsuarioPerfilImg) {
+        Optional<UsuarioPerfilImg> optionalUserProfileImg = repUsuarioPerfilImg.findById(idUsuarioPerfilImg);
+        if (optionalUserProfileImg.isPresent()) {
+            UsuarioPerfilImg userProfileImg = optionalUserProfileImg.get();
+
+            // Deletar o arquivo correspondente à imagem do diretório
+            File imageFile = new File(userProfileImg.getUrl());
+            if (imageFile.exists()) {
+                imageFile.delete();
+            }
+
+            repUsuarioPerfilImg.delete(userProfileImg);
+
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{idUsuarioPerfilImg}")
+    public ResponseEntity<UsuarioPerfilImg> getUserProfileImage(@PathVariable UUID idUsuarioPerfilImg) {
+        Optional<UsuarioPerfilImg> optionalUserProfileImg = repUsuarioPerfilImg.findById(idUsuarioPerfilImg);
+        if (optionalUserProfileImg.isPresent()) {
+            UsuarioPerfilImg userProfileImg = optionalUserProfileImg.get();
+            return ResponseEntity.ok().body(userProfileImg);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+
     private String getUniqueFilename(String originalFilename) {
         String baseName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
         String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
